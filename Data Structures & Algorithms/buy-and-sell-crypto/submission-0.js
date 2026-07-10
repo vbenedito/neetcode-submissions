5db9b44355bd0c4b1d7bb5a64aef05909e1d06f6
vbenedito <beneditoloura@gmail.com>
2026-07-10 07:30:58 -0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let left = 0;
+        let right = 1;
+        let maxProfit = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                let profit = prices[right] - prices[left]
+                maxProfit = Math.max(maxProfit, profit)
+            } else {
+                left = right
+            }
+
+            right++
+        }
+
+        return maxProfit;
+    }
+}
