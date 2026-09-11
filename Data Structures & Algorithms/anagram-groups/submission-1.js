1285d1a4abc6683c380477d94ac560a69aff96bd
vbenedito <beneditoloura@gmail.com>
2026-09-11 11:03:04 -0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = {}
+
+        for (const char of strs) {
+            const sortedStr = [...char].sort((a, b) => a.localeCompare(b)).join("");
+
+            if (!map[sortedStr]) {
+                map[sortedStr] = []
+            }
+
+            map[sortedStr].push(char)
+        }
+
+        // for (const char of strs) {
+        //     const sortedStr = [...char].sort((a, b) => a.localeCompare(b)).join("");
+
+            
+        // }
+
+        return Object.values(map)
+    }
+}
